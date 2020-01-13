10d3d543861c71fc6bfeda566465f6c984edb7f3
chien <dino19121994@gmail.com>
2020-01-13 11:50:53 +0700

    show genre on the home fragment

---
+++ app/src/main/java/com/example/musicproject_01/data/model/Genre.java
@@ -0,0 +1,32 @@
+package com.example.demoprojectmusic.data.model;
+
+public class Genre {
+    private String mTitle;
+    private int mImage;
+
+    public Genre() {
+
+    }
+
+    public Genre(String title, int image) {
+        mTitle = title;
+        mImage = image;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public int getImage() {
+        return mImage;
+    }
+
+    public void setImage(int image) {
+        mImage = image;
+    }
+
+}
