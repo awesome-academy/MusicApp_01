cddb88502f0a72ed2edb4f61d152dc4a4bfdeca1
chien <dino19121994@gmail.com>
2020-01-13 19:02:51 +0700

    show genre on the home fragment

--- app/src/main/java/com/example/musicproject_01/screen/main/home/HomeFragment.java
+++ app/src/main/java/com/example/musicproject_01/screen/main/home/HomeFragment.java
@@ -8,6 +8,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.musicproject_01.R;
 import com.example.musicproject_01.base.BaseFragment;
+import com.example.musicproject_01.constant.GenreEntity;
+import com.example.musicproject_01.data.model.Genre;
+import com.example.musicproject_01.data.model.GenreAdapter;
 import com.example.musicproject_01.data.model.Playlist;
 import com.example.musicproject_01.data.model.PlaylistAdapter;
 import com.example.musicproject_01.data.model.Track;
@@ -25,15 +28,22 @@ public class HomeFragment extends BaseFragment implements HomeContract.View,
         TrackAdapter.OnItemClickListeners,
         TrackDataSource.OnFetchDataListener<Track>,
         PlaylistDataSource.OnFetchDataListener<Playlist>,
-        PlaylistAdapter.OnItemClickPlaylistListeners {
+        PlaylistAdapter.OnItemClickPlaylistListeners,
+        GenreAdapter.OnItemClickGenreListeners {
 
     private HomeContract.Presenter mHomePresenter;
+
     private RecyclerView mRecyclerRecentSong;
     private RecyclerView mRecyclerPlaylist;
+    private RecyclerView mRecyclerGenre;
+
     private TrackAdapter mTrackAdapter;
     private PlaylistAdapter mPlaylistAdapter;
+    private GenreAdapter mGenreAdapter;
+
     private List<Track> mTrackListAdd;
     private List<Playlist> mPlaylistAdd;
+    private List<Genre> mGenreAdd;
 
     private FetchTrackFromUrl mFetchTrackFromUrl;
     private FetchPlaylistFromUrl mFetchPlaylistFromUrl;
@@ -46,18 +56,27 @@ public class HomeFragment extends BaseFragment implements HomeContract.View,
 
     @Override
     protected void initComponents(View view) {
+
         mRecyclerRecentSong = view.findViewById(R.id.recycler_recent_song);
         mRecyclerPlaylist = view.findViewById(R.id.recycler_playlist);
+        mRecyclerGenre = view.findViewById(R.id.recycler_genres);
+
         mFetchTrackFromUrl = new FetchTrackFromUrl();
         mFetchPlaylistFromUrl = new FetchPlaylistFromUrl();
+
         mTrackListAdd = new ArrayList<>();
         mPlaylistAdd = new ArrayList<>();
+        mGenreAdd = new ArrayList<>();
+
+        setupRecyclerViewGenre();
     }
 
     @Override
     protected void registerListeners() {
+
         mFetchTrackFromUrl.setListener(this);
         mFetchPlaylistFromUrl.setListener(this);
+
         mFetchTrackFromUrl.execute(StringUtil.formatTrackAPI());
         mFetchPlaylistFromUrl.execute(StringUtil.formatPlaylistAPI());
     }
@@ -119,9 +138,47 @@ public class HomeFragment extends BaseFragment implements HomeContract.View,
         playlist = mPlaylistAdd.get(position);
     }
 
+    private void setupRecyclerViewGenre() {
+        mGenreAdapter = new GenreAdapter(new ArrayList<Genre>());
+        mGenreAdapter.setOnItemClickGenreListeners(this);
+        mRecyclerGenre.setHasFixedSize(true);
+        mRecyclerGenre.setAdapter(mGenreAdapter);
+        mGenreAdapter.addGenre(addGenre());
+        mGenreAdapter.notifyDataSetChanged();
+
+    }
+
+    private List<Genre> addGenre() {
+        mGenreAdd = new ArrayList<>();
+
+        Genre ambient = new Genre(GenreEntity.AMBIENT_TITLE, R.raw.ambient_genre);
+        Genre alternative = new Genre(GenreEntity.ALTERNATIVE_TITLE, R.raw.alternative_genre);
+        Genre classical = new Genre(GenreEntity.CLASSICAL_TITLE, R.raw.classical_genre);
+        Genre country = new Genre(GenreEntity.COUNTRY_TITLE, R.raw.country_genre);
+        Genre hip_hop = new Genre(GenreEntity.HIP_HOP_TITLE, R.raw.hip_hop);
+        Genre indie = new Genre(GenreEntity.INDIE_TITLE, R.raw.indie_genre);
+        Genre pop = new Genre(GenreEntity.POP_TITLE, R.raw.pop_music);
+        Genre other = new Genre(GenreEntity.OTHER_TITLE, R.raw.other_genre);
+
+        mGenreAdd.add(ambient);
+        mGenreAdd.add(alternative);
+        mGenreAdd.add(classical);
+        mGenreAdd.add(country);
+        mGenreAdd.add(hip_hop);
+        mGenreAdd.add(indie);
+        mGenreAdd.add(pop);
+        mGenreAdd.add(other);
+
+        return mGenreAdd;
+    }
+
+    @Override
+    public void onItemClick(Genre genre, int position) {
+
+    }
+
     @Override
     public void showRecentSong(List<Track> tracks) {
 
     }
-
 }
